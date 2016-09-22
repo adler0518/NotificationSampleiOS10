c6a2f4af4486d5b4400875515925fabdeffd24a5
jinfeng.du <jinfeng.du@qunar.com>
2016-09-22 12:02:06 +0800

    Initial Commit

---
+++ NotificationSampleiOS10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationSampleiOS10
+//
+//  Created by qitmac000260 on 16/9/22.
+//  Copyright © 2016年 jinfeng.du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
