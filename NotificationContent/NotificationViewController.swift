86a231ce38a990d0acc1c4a624a14be5c9564d3f
jinfeng.du <jinfeng.du@qunar.com>
2016-09-22 20:18:30 +0800

    初始化项目

---
+++ NotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContent
+//
+//  Created by qitmac000260 on 16/9/22.
+//  Copyright © 2016年 jinfeng.du. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
---
+++ UIAlertControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertControllerExtension.swift
+//  NotificationSampleiOS10
+//
+//  Created by qitmac000260 on 16/9/22.
+//  Copyright © 2016年 jinfeng.du. All rights reserved.
+//
+
+import Foundation
